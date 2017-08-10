09ca13b9f5665399ed877b02345cc412354ee27c
Ryan Hardeman <Ryan Hardeman>
2017-08-10 07:26:48 -0400

    added friends to array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friendArray = [{
-        name: ,
-        photo: ,
+        name: "Ultron",
+        photo: "https://errantcritic.files.wordpress.com/2015/05/4551282-0585895317-aveng.png",
         scores: [
             "5",
             "1",
@@ -14,8 +14,8 @@ var friendArray = [{
             "1"
         ]
     }, {
-        name: ,
-        photo: ,
+        name: "Tony Stark",
+        photo: "https://vignette4.wikia.nocookie.net/ironman/images/e/ee/Photo%28696%29.jpg/revision/latest?cb=20130922104708",
         scores: [
             "4",
             "2",
@@ -29,8 +29,8 @@ var friendArray = [{
             "2"
         ]
     }, {
-        name: ,
-        photo: ,
+        name: "Blackwidow",
+        photo: "https://19818-presscdn-pagely.netdna-ssl.com/wp-content/uploads/403/0f/bb057433c7770e967e8c28e15c6bb6a2.jpg",
         scores: [
             "4",
             "2",
@@ -45,8 +45,8 @@ var friendArray = [{
         ]
 
     }, {
-        name: ,
-        photo: ,
+        name:"The Vision" ,
+        photo: "http://static6.businessinsider.com/image/555ce33f69bedd276f798015/it-took-35-hours-every-day-to-transform-paul-bettany-into-the-vision-character-for-the-avengers-sequel.jpg",
         scores: [
             "4",
             "2",
